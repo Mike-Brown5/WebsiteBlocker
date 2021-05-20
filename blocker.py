7e368f2887bbab08cb1f57679df7166269c1aa5e
mike-brown5 <mikeaxe5@outlook.com>
2021-05-20 13:24:24 -0400

    added os and a another func

--- blocker.py
+++ blocker.py
@@ -1,16 +1,23 @@
-import time
+import time, os
 from datetime import datetime as dt
 
-host_path = "/etc/hosts" # FOR LINUX!!
-#host_path = "/private/etc/hosts" # FOR MAC!!
-#host_path = "C:\\Windows\\System32\\Drivers\\etc\\hosts" # FOR WINDOWS!!!
+host_pathL = "/etc/hosts" # FOR LINUX!!
+host_pathM = "/private/etc/hosts" # FOR MAC!!
+host_patW = "C:\\Windows\\System32\\Drivers\\etc\\hosts" # FOR WINDOWS!!!
 redirect = "127.0.0.1"
+if os.path.exists(host_pathL) == True:
+            host_path = host_pathL
+elif os.path.exists(host_pathM) == True:
+    host_path = host_pathM
+elif os.path.exists(host_patW) == True:
+    host_path = host_patW
 #Defining the websites list
 websites = ["facebook.com", "youtube.com", "myanimelist.net", "instagram.com", "twitter.com","https://www.youtube.com/","https://www.facebook.com/"]
 
 #Blocking the websites on working hours (8AM:4PM)
 def block():
     while True:
+        
         if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
             with open(host_path, "r+") as file:
                 content = file.read()
--- unblock.py
+++ unblock.py
@@ -1,15 +1,23 @@
-#from blocker import websites
+import os
 websites = ["facebook.com", "youtube.com", "myanimelist.net", "instagram.com", "twitter.com","https://www.youtube.com/","https://www.facebook.com/"]
 
-host_path = "/etc/hosts" # FOR LINUX!!
-#host_path = "/private/etc/hosts" # FOR MAC!!
-#host_path = "C:\\Windows\\System32\\Drivers\\etc\\hosts" # FOR WINDOWS!!!
+host_pathL = "/etc/hosts" # FOR LINUX!!
+host_pathM = "/private/etc/hosts" # FOR MAC!!
+host_patW = "C:\\Windows\\System32\\Drivers\\etc\\hosts" # FOR WINDOWS!!!
+if os.path.exists(host_pathL) == True:
+            host_path = host_pathL
+elif os.path.exists(host_pathM) == True:
+    host_path = host_pathM
+elif os.path.exists(host_patW) == True:
+    host_path = host_patW
+def unblock():
+    with open(host_path, "r+") as file:
+        content = file.readlines()
+        file.seek(0)
+        for web in content:
+            if not any(webs in web for webs in websites):
+                file.write(web)
+        file.truncate()
+        print("unblocked!!")
 
-with open(host_path, "r+") as file:
-    content = file.readlines()
-    file.seek(0)
-    for web in content:
-        if not any(webs in web for webs in websites):
-            file.write(web)
-    file.truncate()
-    print("unblocked!!")
+unblock()
