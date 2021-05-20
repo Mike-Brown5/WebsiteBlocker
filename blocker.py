04e7576db1ca92e1e495e7270911ca084b140a50
mike-brown5 <mikeaxe5@outlook.com>
2021-05-20 12:57:00 -0400

    put it in a func

--- blocker.py
+++ blocker.py
@@ -9,23 +9,26 @@ redirect = "127.0.0.1"
 websites = ["facebook.com", "youtube.com", "myanimelist.net", "instagram.com", "twitter.com","https://www.youtube.com/","https://www.facebook.com/"]
 
 #Blocking the websites on working hours (8AM:4PM)
-while True:
-    if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
-        with open(host_path, "r+") as file:
-            content = file.read()
-            for web in websites:
-                if web in content:
-                    pass
-                else:
-                    file.write(redirect + " "+web + "\n")
-        print("Working...")
-    else:
-        with open(host_path, "r+") as file:
-            content = file.readlines()
-            file.seek(0)
-            for web in content:
-                if not any(webs in web for webs in websites):
-                    file.write(web)
-            file.truncate()
-        print("Not working....")
-    time.sleep(20)
+def block():
+    while True:
+        if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
+            with open(host_path, "r+") as file:
+                content = file.read()
+                for web in websites:
+                    if web in content:
+                        pass
+                    else:
+                        file.write(redirect + " "+web + "\n")
+            print("Working...")
+        else:
+            with open(host_path, "r+") as file:
+                content = file.readlines()
+                file.seek(0)
+                for web in content:
+                    if not any(webs in web for webs in websites):
+                        file.write(web)
+                file.truncate()
+            print("Not working....")
+        time.sleep(20)
+
+block()
